be0f2d6f19934ebd42734ae1d773ce954571bb3d
Loreto Parisi <loretoparisi@gmail.com>
2017-09-22 16:23:49 +0200

    RNN: fixed home path; gpu makefile

--- examples/rnn.c
+++ examples/rnn.c
@@ -128,7 +128,7 @@ void train_char_rnn(char *cfgfile, char *weightfile, char *filename, int clear, 
         fclose(fp);
     }
 
-    char *backup_directory = "/home/pjreddie/backup/";
+    char *backup_directory = getenv("HOME");
     char *base = basecfg(cfgfile);
     fprintf(stderr, "%s\n", base);
     float avg_loss = -1;
