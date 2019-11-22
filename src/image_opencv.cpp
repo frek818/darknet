d78e7cfea9977922eb6c8a35d181ef26c45b8e5e
Herman Griffin <Herman.Griffin@nutonomy.com>
2019-11-22 16:34:20 +0800

    update constant names

--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -56,9 +56,9 @@ void *open_video_stream(const char *f, int c, int w, int h, int fps)
     if(f) cap = new VideoCapture(f);
     else cap = new VideoCapture(c);
     if(!cap->isOpened()) return 0;
-    if(w) cap->set(CAP_PROP_FRAME_WIDTH, w);
-    if(h) cap->set(CAP_PROP_FRAME_HEIGHT, w);
-    if(fps) cap->set(CAP_PROP_FPS, w);
+    if(w) cap->set(CV_CAP_PROP_FRAME_WIDTH, w);
+    if(h) cap->set(CV_CAP_PROP_FRAME_HEIGHT, w);
+    if(fps) cap->set(CV_CAP_PROP_FPS, w);
     return (void *) cap;
 }
 
@@ -107,7 +107,7 @@ void make_window(char *name, int w, int h, int fullscreen)
 {
     namedWindow(name, WINDOW_NORMAL); 
     if (fullscreen) {
-        setWindowProperty(name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
+        setWindowProperty(name, WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
     } else {
         resizeWindow(name, w, h);
         if(strcmp(name, "Demo") == 0) moveWindow(name, 0, 0);
